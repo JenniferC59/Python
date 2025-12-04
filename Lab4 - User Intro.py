037fd8183346f76f4e98d60ae036597398fe8c18
JenniferC59 <cao00059@algonquinlive.com>
2025-12-03 19:06:40 -0500

    Add files via upload

---
+++ Lab4 - User Intro.py
@@ -0,0 +1,13 @@
+# Take a name and print an introduction line
+# Date: September 23, 2025
+
+
+# User to input their name
+name=input("Enter your name: ")
+
+#Def function to print the name entered by the user and an introduction
+def Intro(name) :
+    print(f"Hello, my name is "+ name + ". Nice to meet you.")
+
+# Call function to display name and introduction line.
+Intro(name)
